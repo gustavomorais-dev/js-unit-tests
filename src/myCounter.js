949dd88effa66a23b968433f3463f5e24d78a526
gustavomorais <gustavomorais.dev@gmail.com>
2023-01-23 21:20:20 -0300

    corrige erro de lint no requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counterInner = 2; counterInner <= 3; counterInner += 1) {
+      myArray.push(counterInner);
     }
   }
   return myArray;
